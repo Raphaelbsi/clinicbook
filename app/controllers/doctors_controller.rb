69c7081e3e14a0bdf11c4303c719bee8b42543c6
Raphael Souza de Oliveira <raphaelsouzadeoliveira@MacBook-Pro-de-Raphael.local>
2021-06-14 21:23:23 -0300

    Criados os controllers

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -3,17 +3,36 @@ class DoctorsController < ApplicationController
   end
 
   def index
+    @doctor = doctors
   end
 
   def create
+    @doctor = Doctor.new(doctor_params)
+    respond_to do | format | 
+      if @doctor.save
+        format.json {render json: true}
+      else
+        format.json{render json: @doctor.errors status: :unprocessable_entity}
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @doctor.update(doctor_params)
+        format.json { render json: true }
+      else
+        format.json { render json: @doctor.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
-  end
+    @doctor.destroy
 
-  def raffle
+    respond_to do | format |
+      format.json { render json: true }
+    end
+
   end
 end
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,17 +3,36 @@ class PatientsController < ApplicationController
   end
 
   def index
+    @patient = patients
   end
 
   def create
+    @patient = patient.new(patient_params)
+    respond_to do | format | 
+      if @patient.save
+        format.json {render json: true}
+      else
+        format.json{render json: @patient.errors status: :unprocessable_entity}
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @patient.update(patient_params)
+        format.json { render json: true }
+      else
+        format.json { render json: @patient.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
-  end
+    @patient.destroy
 
-  def raffle
+    respond_to do | format |
+      format.json { render json: true }
+    end
   end
+  
 end
